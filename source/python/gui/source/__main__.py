f9fa7c8063d19d20f2c9b1ae284e3631ec3a3313
JoseSantosAMD <Jose.Santos@amd.com>
2023-03-16 12:42:02 -0500

    multiple verbose modes in terminal set by gui
    -multi selection in sort by is removed, kept in workloads
    -verbose mode 2 shows data from print_speedup_info
    -sort by changed to if/elif
    -removed unused fig
    -fixed errorbars
    -line_stddev now in dataframe

--- source/python/gui/source/__main__.py
+++ source/python/gui/source/__main__.py
@@ -98,14 +98,14 @@ def causal(args):
         input_filters = [
             {
                 "Name": "Verbosity",
-                "values": [0,1,2,3],
-                "default": 0,
+                "values": [0, 1, 2, 3],
+                "default": args.verbose,
                 "type": "Name",
-                "multi": False
+                "multi": False,
             },
             {
                 "Name": "Sort by",
-                "values": list(map(str, sortOptions)),
+                "values": sortOptions,
                 "default": "Impact",
                 "type": "Name",
                 "multi": False,
